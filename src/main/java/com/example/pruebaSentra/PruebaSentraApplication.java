11c4989cfd38679981aa6ef8e3191e47c7d26864
Sergio <sergio.bobadilla@gmail.com>
2024-04-07 21:56:44 -0400

    Add files via upload

---
+++ src/main/java/com/example/pruebaSentra/PruebaSentraApplication.java
@@ -0,0 +1,13 @@
+package com.example.pruebaSentra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaSentraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaSentraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pruebaSentra/PruebaSentraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pruebaSentra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaSentraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
